0316889af9b723867ea41910fe3cc4c0b4530716
IronKinoko <kinoko_main@outlook.com>
2021-04-08 20:17:52 +0800

    feat: paly video

--- index.js
+++ index.js
@@ -1,16 +1,44 @@
-let url = new URLSearchParams(location.search).get('url')
-
-if (url) {
-  let dom = document.querySelector('.empty')
-  dom.style.display = 'none'
-  init()
-}
+let getNodes = (str) =>
+  new DOMParser().parseFromString(str, 'text/html').body.firstChild
 
 function init() {
   let dom = document.getElementById('player')
-  dom.style.display = 'block'
   dom.src = url
   let player = new Plyr(dom, {
     autoplay: true,
   })
+
+  injectNext()
+
+  player.once('ended', () => {
+    notifyParentChangeToNextPart()
+  })
+}
+
+function injectNext() {
+  let dom = document.getElementById('controls')
+  let plEl = document.querySelector(
+    '.plyr__controls__item.plyr__progress__container'
+  )
+  let pNode = document.querySelector('.plyr__controls')
+
+  let nextNode = getNodes(dom.innerHTML)
+  nextNode.addEventListener('click', () => {
+    notifyParentChangeToNextPart()
+  })
+  pNode.insertBefore(nextNode, plEl)
+}
+
+function notifyParentChangeToNextPart() {
+  if (parent !== self) {
+    parent.postMessage({ code: 233, message: 'next part' })
+  }
+}
+
+let url = new URLSearchParams(location.search).get('url')
+
+if (url) {
+  let dom = document.querySelector('.empty')
+  dom.remove()
+  init()
 }
