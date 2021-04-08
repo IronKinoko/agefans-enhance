e2bb1096d6d571fb49e3a05926c954881dd3236f
IronKinoko <kinoko_main@outlook.com>
2021-04-08 21:09:50 +0800

    feat: toggle fullscreen

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let getNodes = (str) =>
   new DOMParser().parseFromString(str, 'text/html').body.firstChild
 
+let isFull = false
 function init() {
   let dom = document.getElementById('player')
   dom.src = url
@@ -8,31 +9,68 @@ function init() {
     autoplay: true,
   })
 
-  injectNext()
-
+  if (parent !== self) {
+    injectNext()
+    injectSreen()
+  }
   player.once('ended', () => {
     notifyParentChangeToNextPart()
+  })
+
+  player.on('enterfullscreen', () => {
+    toggleFullscreen(false)
   })
 }
 
 function injectNext() {
-  let dom = document.getElementById('controls')
-  let plEl = document.querySelector(
+  const dom = document.getElementById('plyr__next')
+  const plEl = document.querySelector(
     '.plyr__controls__item.plyr__progress__container'
   )
-  let pNode = document.querySelector('.plyr__controls')
+  const pNode = document.querySelector('.plyr__controls')
 
-  let nextNode = getNodes(dom.innerHTML)
+  const nextNode = getNodes(dom.innerHTML)
   nextNode.addEventListener('click', () => {
     notifyParentChangeToNextPart()
   })
   pNode.insertBefore(nextNode, plEl)
 }
 
-function notifyParentChangeToNextPart() {
-  if (parent !== self) {
-    parent.postMessage({ code: 233, message: 'next part' })
+function injectSreen() {
+  const dom = document.getElementById('plyr__fullscreen')
+  const plEl = document.querySelector('[data-plyr="fullscreen"]')
+  const pNode = document.querySelector('.plyr__controls')
+
+  const nextNode = getNodes(dom.innerHTML)
+  nextNode.addEventListener('click', (e) => {
+    toggleFullscreen()
+  })
+  pNode.insertBefore(nextNode, plEl)
+}
+
+function toggleFullscreen(bool) {
+  if (typeof bool === 'boolean') {
+    isFull = bool
+  } else {
+    isFull = !isFull
   }
+
+  let use = document.querySelector('.plyr__fullscreen.plyr__custom use')
+  if (isFull) {
+    use.setAttribute('xlink:href', '#fullscreen-quit')
+  } else {
+    use.setAttribute('xlink:href', '#fullscreen')
+  }
+
+  notifyParentChangeScreenSize()
+}
+
+function notifyParentChangeToNextPart() {
+  parent.postMessage({ code: 233, message: 'next part' })
+}
+
+function notifyParentChangeScreenSize() {
+  parent.postMessage({ code: 666, message: 'change size', isFull })
 }
 
 let url = new URLSearchParams(location.search).get('url')
