a2b7f4f4e2e7d10fe4eb51ee22c4b3ca15155629
IronKinoko <kinoko_main@outlook.com>
2022-02-05 11:41:15 +0800

    chore: 更新jquery依赖

--- template/userscript.js
+++ template/userscript.js
@@ -14,7 +14,7 @@ export function genUserScriptInfo(pkg) {
 // @include      http://www.imomoe.live/player/*
 // @include      http://www.88dmw.com/*
 // @run-at       document-body
-// @require      https://cdn.jsdelivr.net/npm/jquery@1.12.4/dist/jquery.min.js
+// @require      https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js
 // @require      https://cdn.jsdelivr.net/npm/plyr@3.6.4/dist/plyr.min.js
 // @require      https://cdn.jsdelivr.net/npm/hls.js@1.0.9/dist/hls.min.js
 // @grant        GM_getValue
