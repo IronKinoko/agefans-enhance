0d2e14ae74dbc3cdbced2ca0390a38b721b7d507
IronKinoko <kinoko_main@outlook.com>
2022-02-16 22:28:17 +0800

    refactor: 重写 88dmw 播放逻辑

--- src/adapter/88dmw/play.ts
+++ src/adapter/88dmw/play.ts
@@ -1,31 +1,10 @@
 import $ from 'jquery'
 import { KPlayer } from '../../player'
-import parseToURL from '../../utils/parseToURL'
-
-let player: KPlayer
 
 function replacePlayer() {
-  const dom = document.querySelector<HTMLIFrameElement>(
-    '#playleft iframe[allowfullscreen="true"]'
-  )!
-  const fn = () => {
-    if (!dom.src) return
-    let url = new URL(dom.src)
-    let videoURL = url.searchParams.get('url')
-    if (videoURL) {
-      player = new KPlayer('#beyond-play-box')
-      player.src = parseToURL(videoURL)
-      initEvent()
-      mutationOb.disconnect()
-    }
-  }
+  let player = new KPlayer('#beyond-play-box')
+  player.src = unsafeWindow.MacPlayer.PlayUrl
 
-  const mutationOb = new MutationObserver(fn)
-  mutationOb.observe(dom, { attributes: true })
-  fn()
-}
-
-function initEvent() {
   player.on('prev', () => unsafeWindow.MacPlayer.GoPreUrl())
   player.on('next', () => unsafeWindow.MacPlayer.GoNextUrl())
 }
