30849decd98188cf35eacc85f85dd833e107cc4b
IronKinoko <kinoko_main@outlook.com>
2022-07-20 22:56:28 +0800

    feat: bangumi.online support danmaku

--- src/adapter/newani/index.ts
+++ src/adapter/newani/index.ts
@@ -4,4 +4,8 @@ import { playModule } from './play'
 runtime.register({
   domains: ['new-ani.me', 'bangumi.online'],
   opts: [{ test: '/watch', run: playModule }],
+  search: {
+    getSearchName: () => $('.watch-content-info-text-title-name').text(),
+    getEpisode: () => $('.watch-right-series-block-volumes-active').text(),
+  },
 })
