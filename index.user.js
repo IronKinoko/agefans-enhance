2e3ebf662bd95d0cbb7d8034f2f094dbd107342c
IronKinoko <kinoko_main@outlook.com>
2022-07-20 22:58:16 +0800

    Updates

--- index.user.js
+++ index.user.js
@@ -2,7 +2,7 @@
 // @name         agefans Enhance
 // @namespace    https://github.com/IronKinoko/agefans-enhance
 // @icon         https://www.agemys.com/favicon.ico
-// @version      1.27.6
+// @version      1.27.7
 // @description  增强agefans播放功能，实现自动换集、无缝换集、画中画、历史记录、断点续播、弹幕等功能
 // @author       IronKinoko
 // @include      https://www.age.tv/*
@@ -1757,7 +1757,7 @@
         content: `
     <table>
       <tbody>
-      <tr><td>\u811A\u672C\u7248\u672C</td><td>${"1.27.6"}</td></tr>
+      <tr><td>\u811A\u672C\u7248\u672C</td><td>${"1.27.7"}</td></tr>
       <tr>
         <td>\u811A\u672C\u4F5C\u8005</td>
         <td><a target="_blank" rel="noreferrer" href="https://github.com/IronKinoko">IronKinoko</a></td>
@@ -1854,7 +1854,7 @@ ${src}
 
 # \u73AF\u5883
 userAgent: ${navigator.userAgent}
-\u811A\u672C\u7248\u672C: ${"1.27.6"}
+\u811A\u672C\u7248\u672C: ${"1.27.7"}
 `;
 
   const GlobalKey = "show-help-info";
@@ -3941,7 +3941,11 @@ ${[...speedList].reverse().map((speed) => `<li class="k-menu-item k-speed-item" 
 
   runtime.register({
     domains: ["new-ani.me", "bangumi.online"],
-    opts: [{ test: "/watch", run: playModule$6 }]
+    opts: [{ test: "/watch", run: playModule$6 }],
+    search: {
+      getSearchName: () => $(".watch-content-info-text-title-name").text(),
+      getEpisode: () => $(".watch-right-series-block-volumes-active").text()
+    }
   });
 
   async function playModule$5() {
