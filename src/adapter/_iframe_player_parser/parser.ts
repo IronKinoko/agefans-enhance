356097e5a3db88ddb39c9ca744844d2473403544
IronKinoko <kinoko_main@outlook.com>
2022-10-04 14:58:09 +0800

    fix: yhdmjx video src decrypt

--- src/adapter/_iframe_player_parser/parser.ts
+++ src/adapter/_iframe_player_parser/parser.ts
@@ -6,8 +6,15 @@ let player: KPlayer
 export const parser = {
   'danmu.yhdmjx.com': async () => {
     const video = await queryDom<HTMLVideoElement>('video')
+    video.src = ''
 
-    player = new KPlayer('#player', { video, eventToParentWindow: true })
+    player = new KPlayer('#player', { eventToParentWindow: true })
+
+    player.src = unsafeWindow.v_decrypt(
+      unsafeWindow.config.url,
+      unsafeWindow._token_key,
+      unsafeWindow.key_token
+    )
   },
   'pro.ascepan.top': async () => {
     const video = await queryDom<HTMLVideoElement>('video')
