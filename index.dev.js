ea0b3704305c570d03c2f446837173eb1c67ded6
IronKinoko <kinoko_main@outlook.com>
2023-05-27 20:31:32 +0800

    Updates

--- index.dev.js
+++ index.dev.js
@@ -2,13 +2,13 @@
 // @name         agefans Enhance - Dev
 // @namespace    https://github.com/IronKinoko/agefans-enhance
 // @icon         https://www.agemys.com/favicon.ico
-// @version      1.36.10
+// @version      1.36.11
 // @description  增强agefans播放功能，实现自动换集、无缝换集、画中画、历史记录、断点续播、弹幕等功能
 // @author       IronKinoko
 // @include      https://www.age.tv/*
 // @include      https://www.agefans.*
 // @include      https://www.agemys.*
-// @include      http://www.yinghuacd.com/*
+// @include      http*://*.yinghuacd.com/*
 // @include      https://www.yhdmp.net/vp/*
 // @include      https://bangumi.online/*
 // @include      http*://www.ntdm8.*
