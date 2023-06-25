c3f927b3f96e4e783c2c7f95760d5036dc81c994
IronKinoko <kinoko_main@outlook.com>
2023-06-25 09:01:40 +0800

    Updates

--- index.user.js
+++ index.user.js
@@ -2,13 +2,13 @@
 // @name         agefans Enhance
 // @namespace    https://github.com/IronKinoko/agefans-enhance
 // @icon         https://www.agemys.com/favicon.ico
-// @version      1.36.12
+// @version      1.36.10
 // @description  增强agefans播放功能，实现自动换集、无缝换集、画中画、历史记录、断点续播、弹幕等功能
 // @author       IronKinoko
 // @include      https://www.age.tv/*
 // @include      https://www.agefans.*
 // @include      https://www.agemys.*
-// @include      http*://*.yinghuacd.com/*
+// @include      http://www.yinghuacd.com/*
 // @include      https://www.yhdmp.net/vp/*
 // @include      https://bangumi.online/*
 // @include      http*://www.ntdm8.*
@@ -1328,7 +1328,7 @@
         content: `
     <table>
       <tbody>
-      <tr><td>\u811A\u672C\u7248\u672C</td><td>${"1.36.12"}</td></tr>
+      <tr><td>\u811A\u672C\u7248\u672C</td><td>${"1.36.10"}</td></tr>
       <tr>
         <td>\u811A\u672C\u4F5C\u8005</td>
         <td><a target="_blank" rel="noreferrer" href="https://github.com/IronKinoko">IronKinoko</a></td>
@@ -1433,7 +1433,7 @@ ${src}
 
 # \u73AF\u5883
 userAgent: ${navigator.userAgent}
-\u811A\u672C\u7248\u672C: ${"1.36.12"}
+\u811A\u672C\u7248\u672C: ${"1.36.10"}
 `;
 
   const GlobalKey = "show-help-info";
@@ -2037,7 +2037,6 @@ ${[...speedList].reverse().map(
       });
       this.on("loadedmetadata", () => {
         this.$loading.hide();
-        this.$searchActions.find(".k-text-btn-text").text(this.media.videoHeight + "P");
       });
       this.on("canplay", () => {
         this.$loading.hide();
