86594a6c2f89f743fd1c74c81279fe7581e4d24c
IronKinoko <kinoko.main@gmail.com>
2024-04-27 02:34:50 +0800

    Updates

--- index.user.js
+++ index.user.js
@@ -2,7 +2,7 @@
 // @name         agefans Enhance
 // @namespace    https://github.com/IronKinoko/agefans-enhance
 // @icon         https://www.agemys.com/favicon.ico
-// @version      1.39.8
+// @version      1.39.9
 // @description  增强agefans播放功能，实现自动换集、无缝换集、画中画、历史记录、断点续播、弹幕等功能
 // @author       IronKinoko
 // @include      https://www.age.tv/*
@@ -2033,7 +2033,7 @@
         content: `
     <table>
       <tbody>
-      <tr><td>\u811A\u672C\u7248\u672C</td><td>${"1.39.8"}</td></tr>
+      <tr><td>\u811A\u672C\u7248\u672C</td><td>${"1.39.9"}</td></tr>
       <tr>
         <td>\u811A\u672C\u4F5C\u8005</td>
         <td><a target="_blank" rel="noreferrer" href="https://github.com/IronKinoko">IronKinoko</a></td>
@@ -2138,7 +2138,7 @@ ${src}
 
 # \u73AF\u5883
 userAgent: ${navigator.userAgent}
-\u811A\u672C\u7248\u672C: ${"1.39.8"}
+\u811A\u672C\u7248\u672C: ${"1.39.9"}
 `;
 
   const GlobalKey = "show-help-info";
@@ -2208,7 +2208,10 @@ userAgent: ${navigator.userAgent}
           start = this.currentTime;
           this.message.info("\u5F00\u59CB\u8BB0\u5F55\u81EA\u5B9A\u4E49\u8DF3\u8F6C\u65F6\u95F4");
         } else {
-          this.localConfig.customSeekTime = Math.round(this.currentTime - start);
+          this.configSaveToLocal(
+            "customSeekTime",
+            Math.abs(Math.round(this.currentTime - start))
+          );
           this.message.info(
             `\u8BB0\u5F55\u6210\u529F\uFF0C\u81EA\u5B9A\u4E49\u8DF3\u8F6C\u65F6\u95F4\u4E3A${this.localConfig.customSeekTime}s`
           );
