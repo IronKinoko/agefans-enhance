df2fbe9dcde2c78d2f16d41c298019c61e56e5f7
IronKinoko <kinoko.main@gmail.com>
2025-01-24 17:58:21 +0800

    fix: update search URL for cycanime

--- src/adapter/cycanime/index.ts
+++ src/adapter/cycanime/index.ts
@@ -15,7 +15,7 @@ runtime.register({
   ],
   search: {
     name: '次元城',
-    search: (name) => `https://www.cyc-anime.net/search.html?wd=${name}`,
+    search: (name) => `https://www.cycani.org/search.html?wd=${name}`,
     getSearchName: () => {
       return new Promise((resolve) => {
         const fn = (e: MessageEvent<any>) => {
