d1de7589dd75525ed6e8e1f2f290d222f0077f3e
IronKinoko <kinoko.main@gmail.com>
2025-11-08 09:42:25 +0800

    Updates

--- index.user.js
+++ index.user.js
@@ -2,7 +2,7 @@
 // @name         agefans Enhance
 // @namespace    https://github.com/IronKinoko/agefans-enhance
 // @icon         https://www.age.tv/favicon.ico
-// @version      1.53.4
+// @version      1.53.5
 // @description  增强播放功能，实现自动换集、无缝换集、画中画、历史记录、断点续播、弹幕等功能。适配agefans、NT动漫、bimiacg、mutefun、次元城、稀饭动漫
 // @author       IronKinoko
 // @include      https://www.age.tv/*
@@ -2644,7 +2644,7 @@
         content: `
     <table class="k-table">
       <tbody>
-      <tr><td>\u811A\u672C\u7248\u672C</td><td>${"1.53.4"}</td></tr>
+      <tr><td>\u811A\u672C\u7248\u672C</td><td>${"1.53.5"}</td></tr>
       <tr>
         <td>\u811A\u672C\u4F5C\u8005</td>
         <td><a target="_blank" rel="noreferrer" href="https://github.com/IronKinoko">IronKinoko</a></td>
@@ -2770,7 +2770,7 @@ ${src}
 
 # \u73AF\u5883
 userAgent: ${navigator.userAgent}
-\u811A\u672C\u7248\u672C: ${"1.53.4"}
+\u811A\u672C\u7248\u672C: ${"1.53.5"}
 `;
 
   const GlobalKey = "show-help-info";
@@ -7038,7 +7038,7 @@ ${text}
     ],
     search: {
       name: "girigiri\u7231",
-      search: (cn) => `https://anime.girigirilove.com/search/-------------/?wd=${cn}`,
+      search: (cn) => `https://bgm.girigirilove.com/search/-------------/?wd=${cn}`,
       getSearchName: () => {
         return new Promise((resolve) => {
           const fn = (e) => {
