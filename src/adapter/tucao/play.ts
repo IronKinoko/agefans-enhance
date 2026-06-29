8a3d4c7f8bd1a9cff648e2f682955fa43432a5b6
IronKinoko <kinoko.main@gmail.com>
2026-06-29 23:16:38 +0800

    feat: 确保tucao video不会重叠声音

--- src/adapter/tucao/play.ts
+++ src/adapter/tucao/play.ts
@@ -7,7 +7,15 @@ export function main() {
 
 async function replacePlayer() {
   await wait(() => !!$('.dplayer-video-wrap video').attr('src'))
-  const url = $('.dplayer-video-wrap video').attr('src')!
+
+  const video = $('.dplayer-video-wrap video')[0] as HTMLVideoElement
+  const url = video.src
+
+  // 防止原生播放器继续播放，导致声音叠加
+  video.pause()
+  video.muted = true
+  video.src = ''
+  video.remove()
 
   const player = new KPlayer('#v_link_p')
   player.src = url
